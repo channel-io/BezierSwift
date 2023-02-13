8fbf47b82efe4b565cceae8a9a1d0eb4ae864c7a
jam0128 <52900717+jam0128@users.noreply.github.com>
2023-02-13 21:36:31 +0900

    [Feature] 베지어 버튼, Boxing 관련 기능 추가 (#5)
    * [Feature] 베지어 버튼, Boxing 관련 기능 추가
    
    * [Etc] 리뷰적용

--- Sources/BezierSwift/Foundation/Color/SemanticColor.swift
+++ Sources/BezierSwift/Foundation/Color/SemanticColor.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 public enum SemanticColor {
+  case bgTransparent
+  
   // MARK: - Background
   case bgWhiteHigh
   case bgWhiteLow
@@ -155,6 +157,7 @@ extension SemanticColor {
   
   private var paletteSet: PaletteSet {
     switch self {
+    case .bgTransparent: return (Palette.white_0, Palette.white_0)
       // MARK: - Background
     case .bgWhiteHigh: return (Palette.white, Palette.grey700)
     case .bgWhiteLow: return (Palette.white, Palette.grey800)
