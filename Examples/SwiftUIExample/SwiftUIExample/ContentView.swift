edd87465afffc1a939c8b7bd22daefe9ffd71efe
Tom Solchan Ahn <tom@channel.io>
2023-06-28 00:26:35 +0900

    [Feature] SwiftUIExample 추가

---
+++ Examples/SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Tom on 2023/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
