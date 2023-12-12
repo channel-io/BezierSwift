8903da29b4b95710e5d7d68da50a9d51139fb6d7
heoblitz <woodyheo12@gmail.com>
2023-12-12 13:42:44 +0900

    [Etc] 컨테이터 뷰 zIndex 설정
    [description]
    
    [issue-number]

--- Sources/BezierSwift/BezierContainerView.swift
+++ Sources/BezierSwift/BezierContainerView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
+private enum ZIndex {
+  static let toast = 1.0
+  static let dialog = 2.0
+}
+
 public struct BezierContainerView: View, Themeable {
   @Environment(\.colorScheme) public var colorScheme
   
   public var body: some View {
     ZStack {
-      BezierDialogContainerView(viewModel: BezierSwift.shared.dialogViewModel)
       BezierToastContainerView(viewModel: BezierSwift.shared.toastViewModel)
+        .zIndex(ZIndex.toast)
+      BezierDialogContainerView(viewModel: BezierSwift.shared.dialogViewModel)
+        .zIndex(ZIndex.dialog)
     }
   }
 }
