638cb109a861a8f20276e8995f8924b3a1188dbd
Tom Solchan Ahn <tom@channel.io>
2023-12-20 23:24:17 +0900

    [Feature] 리디자인용 Example App 추가

---
+++ Examples/RedesignSwiftUIExample/RedesignSwiftUIExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RedesignSwiftUIExample
+//
+//  Created by Tom on 12/20/23.
+//
+
+import SwiftUI
+import RedesignBezierSwift
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      TestView()
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
