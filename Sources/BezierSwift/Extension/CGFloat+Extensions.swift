816118083ceedbdbd46c9fe1b0ce6bbb7b3e2c50
heoblitz <woodyheo12@gmail.com>
2024-01-08 15:37:03 +0900

    [Etc] int extension LiteralType 으로 변경
    [description]
    
    [issue-number]

---
+++ Sources/BezierSwift/Extension/CGFloat+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  CGFloat+Extensions.swift
+//
+//
+//  Created by woody on 12/11/23.
+//
+
+import Foundation
+
+extension IntegerLiteralType {
+  var f: CGFloat { CGFloat(self) }
+}
+
+extension FloatLiteralType {
+  var f: CGFloat { CGFloat(self) }
+}
