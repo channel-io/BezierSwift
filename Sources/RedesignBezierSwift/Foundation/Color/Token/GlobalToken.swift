de508555698afa5939af88dea02b455bd73f4dfd
Tom Solchan Ahn <tom@channel.io>
2024-03-20 16:19:02 +0900

    [Feature] GlobalToken 추가

---
+++ Sources/RedesignBezierSwift/Foundation/Color/Token/GlobalToken.swift
@@ -0,0 +1,146 @@
+//
+//  GlobalToken.swift
+//
+//
+//  Created by Tom on 3/20/24.
+//
+
+import Foundation
+
+struct GlobalToken {
+  var hex: String
+  
+  init(hex: String) {
+    self.hex = hex
+  }
+}
+
+// MARK: - Blue
+extension GlobalToken {
+  
+}
+
+// MARK: - Cobalt
+extension GlobalToken {
+  
+}
+
+// MARK: - Green
+extension GlobalToken {
+  
+}
+
+// MARK: - Red
+extension GlobalToken {
+  
+}
+
+// MARK: - Orange
+extension GlobalToken {
+  
+}
+
+// MARK: - Yellow
+extension GlobalToken {
+  
+}
+
+// MARK: - Olive
+extension GlobalToken {
+  
+}
+
+// MARK: - Teal
+extension GlobalToken {
+  
+}
+
+// MARK: - Navy
+extension GlobalToken {
+  
+}
+
+// MARK: - Purple
+extension GlobalToken {
+  
+}
+
+// MARK: - Pink
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadeblue
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadecobalt
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadegreen
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadered
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadeorange
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadeyellow
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadeolive
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadeteal
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadenavy
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadepurple
+extension GlobalToken {
+  
+}
+
+// MARK: - Shadepink
+extension GlobalToken {
+  
+}
+
+// MARK: - White
+extension GlobalToken {
+  
+}
+
+// MARK: - Black
+extension GlobalToken {
+  
+}
+
+// MARK: - Grey(Solid)
+extension GlobalToken {
+  
+}
+
+// MARK: - Grey(Alpha)
+extension GlobalToken {
+  
+}
