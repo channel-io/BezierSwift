e46e141b5ee0f65396b4151c4da946bd85e223c5
Tom Solchan Ahn <tom@channel.io>
2024-05-29 15:14:36 +0900

    [Improvement] 베지어 폰트 컬러 추가 (#34)

--- Sources/RedesignBezierSwift/Foundation/BezierTheme.swift
+++ Sources/RedesignBezierSwift/Foundation/BezierTheme.swift
@@ -5,9 +5,23 @@
 //  Created by Tom on 4/24/24.
 //
 
-import Foundation
+import SwiftUI
 
 public enum BezierTheme {
   case light
   case dark
+}
+
+public protocol Themeable {
+  var colorScheme: ColorScheme { get }
+}
+
+extension Themeable {
+  public func palette(_ bezierColor: BezierColor) -> Color {
+    switch self.colorScheme {
+    case .light: return bezierColor.color(for: .light)
+    case .dark: return bezierColor.color(for: .dark)
+    @unknown default: return bezierColor.color(for: .light)
+    }
+  }
 }
--- Sources/RedesignBezierSwift/Foundation/Typography/Typography.swift
+++ Sources/RedesignBezierSwift/Foundation/Typography/Typography.swift
@@ -9,17 +9,26 @@ import SwiftUI
 
 extension View {
   public func applyBezierFontStyle(
-    _ bezierFont: BezierFont
+    _ bezierFont: BezierFont,
+    bezierColor: BezierColor
   ) -> some View {
-    self.modifier(BezierFontStyle(bezierFont: bezierFont))
+    self.modifier(BezierFontStyle(bezierFont: bezierFont, bezierColor: bezierColor))
   }
 }
 
-private struct BezierFontStyle: ViewModifier {
-  let bezierFont: BezierFont
+private struct BezierFontStyle: ViewModifier, Themeable {
+  @Environment(\.colorScheme) fileprivate var colorScheme
+  private let bezierFont: BezierFont
+  private let bezierColor: BezierColor
+  
+  init(bezierFont: BezierFont, bezierColor: BezierColor) {
+    self.bezierFont = bezierFont
+    self.bezierColor = bezierColor
+  }
   
   func body(content: Content) -> some View {
     content
       .font(self.bezierFont.font)
+      .foregroundColor(self.palette(self.bezierColor))
   }
 }
