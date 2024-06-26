84b50406bcdc26eb61dcd05c13200f56dfe5fb9e
구본욱 Finn <bnoo1333@gmail.com>
2024-06-26 14:09:31 +0900

    [MOB-1104] 컴포넌트 구현에 필요한 외부 라이브러리 의존성 추가 (#48)
    * [Feature] SnapKit과 SDWebImage 의존성 추가
    
    * [Fix] 필요없는 의존성 제거

--- Package.swift
+++ Package.swift
@@ -20,13 +20,16 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.1")),
+    .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.14.2")),
+    .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "3.0.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "BezierSwift",
-      dependencies: [],
+      dependencies: ["SnapKit", "SDWebImage", "SDWebImageSwiftUI"],
       path: "BezierSwift",
       sources: ["Sources"],
       resources: [.process("Resources")]
