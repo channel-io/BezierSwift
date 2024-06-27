58372ecc8063b809fb8da15b00584cfe1b992d43
구본욱 Finn <bnoo1333@gmail.com>
2024-06-27 13:26:06 +0900

    [Etc] 버튼 컨피규레이션의 변경에 따른 UI변화를 볼수 있도록 UIKitExample 프로젝트의 화면에 추가

--- Examples/UIKitExample/UIKitExample/ViewController.swift
+++ Examples/UIKitExample/UIKitExample/ViewController.swift
@@ -7,9 +7,69 @@
 
 import UIKit
 import BezierSwift
+import SnapKit
 
 final class ViewController: UIViewController {
+  private var clickCount = 1
+
+  lazy var bezierButton = UIBezierButton(
+    configuration: BezierButtonConfiguration(
+      text: "퓨어 버튼 \(self.clickCount)",
+      variant: .primary,
+      color: .blue
+    )
+  )
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    self.view.addSubview(self.bezierButton)
+
+    self.bezierButton.snp.makeConstraints {
+      $0.center.equalToSuperview()
+    }
+
+    self.bezierButton.addAction(
+      UIAction { _ in  self.bezierButtonTapped() },
+      for: .touchUpInside
+    )
+  }
+
+  private func bezierButtonTapped() {
+    self.bezierButton.isLoading = true
+    let remainder = self.clickCount % 3
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+      self.clickCount += 1
+
+      self.bezierButton.update { configuration in
+        switch remainder {
+        case 0:
+          configuration.variant = .secondary
+          configuration.color = .blue
+          configuration.size = .large
+          configuration.text = "Secondary 블루 라지 (\(self.clickCount))"
+          configuration.prefixContent = .icon(.bot)
+          configuration.suffixContent = .icon(.api)
+        case 1:
+          configuration.variant = .tertiary
+          configuration.color = .cobalt
+          configuration.size = .small
+          configuration.text = "Tertiary 코발트 스몰 (\(self.clickCount))"
+          configuration.prefixContent = .icon(.wifi)
+          configuration.suffixContent = nil
+        case 2:
+          configuration.variant = .primary
+          configuration.color = .orange
+          configuration.size = .medium
+          configuration.text = "Primary 오랜지 미디엄 (\(self.clickCount))"
+          configuration.prefixContent = nil
+          configuration.suffixContent = .icon(.chatBubble)
+        default: break
+        }
+      }
+
+      self.bezierButton.isLoading = false
+    }
   }
 }
