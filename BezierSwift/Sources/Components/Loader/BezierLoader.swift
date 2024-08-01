4781ac1ccff90ccd6c510592b5eb724c047ef73b
Tom Solchan Ahn <tom@channel.io>
2024-08-01 17:00:56 +0900

    [Feature] SwiftUI Loader 를 UIKit Loader로 전환

--- BezierSwift/Sources/Components/Loader/BezierLoader.swift
+++ BezierSwift/Sources/Components/Loader/BezierLoader.swift
@@ -7,19 +7,26 @@
 
 import SwiftUI
 
-private enum Constant {
-  static let delayPoint: Double = 0.8
-  static let maxFillPoint: Double = 1.0
-  static let animationDuration: Double = 2.0
-  static let fullCircle: Double = 360
-  static let minAngle: Double = 1
-  static let maxAngle: Double = 359
+/// NOTE: SwiftUI 로 구현한 BezierLoader의 경우 Animation 사이클이 부모뷰 사이클과 같이 동작하기 때문에 범용적인 컴포넌트로 사용이 어려운 문제가 있었습니다.
+/// UIKit으로 구현한 Loader를 UIViewRepresentable 로 감싸서 사용하며, 정확한 사이즈 사용을 위해 SwiftUI View로 한번 더 감싸서 사용합니다. by Tom 2024.08.01
+private struct _BezierLoader: UIViewRepresentable {
+  private let configuration: BezierLoaderConfiguration
+  
+  init(configuration: BezierLoaderConfiguration) {
+    self.configuration = configuration
+  }
+  
+  func makeUIView(context: Context) -> UIBezierLoader {
+    let loader = UIBezierLoader(configuration: self.configuration)
+    return loader
+  }
+  
+  func updateUIView(_ loader: UIBezierLoader, context: Context) {
+    
+  }
 }
 
 public struct BezierLoader: View {
-  @State private var fillPoint = 0.0
-  @State private var isRotating = false
-  
   private let configuration: BezierLoaderConfiguration
   
   public init(configuration: BezierLoaderConfiguration) {
@@ -27,100 +34,9 @@ public struct BezierLoader: View {
   }
   
   public var body: some View {
-    ZStack {
-      Track(configuration: self.configuration)
-        .stroke(self.configuration.trackColor.color, lineWidth: self.configuration.lineWidth)
-      
-      Indicator(fillPoint: self.fillPoint, configuration: self.configuration)
-        .stroke(
-          self.configuration.indicatorColor.color,
-          style: StrokeStyle(lineWidth: self.configuration.lineWidth, lineCap: .round)
-        )
-        .rotationEffect(.degrees(-90))
-        .rotationEffect(Angle(degrees: self.isRotating ? Constant.fullCircle : .zero))
-        .onAppear() {
-          withAnimation(
-            Animation.timingCurve(0.4, 0.0, 0.2, 1.0, duration: Constant.animationDuration)
-              .repeatForever(autoreverses: false)
-          ) {
-            self.fillPoint = Constant.maxFillPoint
-          }
-          
-          withAnimation(
-            Animation.linear(duration: Constant.animationDuration)
-              .repeatForever(autoreverses: false)
-          ) {
-            self.isRotating = true
-          }
-        }
-    }
-    .frame(
-      width: self.configuration.loaderSize.width,
-      height: self.configuration.loaderSize.height
-    )
-  }
-}
-
-private struct Indicator: Shape {
-  var fillPoint: Double
-  let configuration: BezierLoaderConfiguration
-  
-  var animatableData: Double {
-    get { return self.fillPoint }
-    set { self.fillPoint = newValue }
-  }
-  
-  init(fillPoint: Double = 1, configuration: BezierLoaderConfiguration) {
-    self.fillPoint = fillPoint
-    self.configuration = configuration
-  }
-  
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    
-    let startAngle = self.fillPoint > Constant.delayPoint
-    ? ((self.fillPoint - Constant.delayPoint) / (1.0 - Constant.delayPoint)) * Constant.fullCircle
-    : .zero
-    
-    let endAngle = Constant.fullCircle * self.fillPoint
-    
-    path.addArc(
-      center: CGPoint(
-        x: self.configuration.loaderSize.width / 2,
-        y: self.configuration.loaderSize.height / 2
-      ),
-      radius: self.configuration.loaderRadius,
-      startAngle: .degrees(min(startAngle, Constant.maxAngle)),
-      endAngle: .degrees(max(endAngle, Constant.minAngle)),
-      clockwise: false
-    )
-    
-    return path
-  }
-}
-
-private struct Track: Shape {
-  let configuration: BezierLoaderConfiguration
-  
-  init(configuration: BezierLoaderConfiguration) {
-    self.configuration = configuration
-  }
-  
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    
-    path.addArc(
-      center: CGPoint(
-        x: self.configuration.loaderSize.width / 2,
-        y: self.configuration.loaderSize.height / 2
-      ),
-      radius: self.configuration.loaderRadius,
-      startAngle: .degrees(.zero),
-      endAngle: .degrees(Constant.fullCircle),
-      clockwise: false
-    )
-    
-    return path
+    _BezierLoader(configuration: self.configuration)
+      .compositingGroup()
+      .frame(length: self.configuration.loaderLength)
   }
 }
 
