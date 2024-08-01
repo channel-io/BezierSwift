19e4204ec22d933946eafca667a9a28ba0d3e35a
Tom Solchan Ahn <tom@channel.io>
2024-08-01 17:01:34 +0900

    [Feature] Tintable 추가

--- BezierSwift/Sources/Foundation/Common/Tintable.swift
+++ BezierSwift/Sources/Foundation/Common/Tintable.swift
@@ -19,6 +19,10 @@ extension EnvironmentValues {
 }
 
 extension View {
+  /// 작업의 진행 단계 결정 또는 완료 시점이 확정되지 않은 경우 사용할 수 있는 요소입니다.
+  /// - Parameters:
+  ///   - bezierColor: 부모 뷰에서 정의한 `BezierColor`를 environment 로 전달하여 TintColor를 적용합니다.
+  /// - Attention:TintColor가 적용되는 자식뷰의 UI에는 반드시 `tintable()` 선언해야 합니다.
   @ViewBuilder
   func applyTintBezierColor(_ bezierColor: BezierColor) -> some View {
     self.environment(\.tintBezierColor, bezierColor)
