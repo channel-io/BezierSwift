e7eedbc31efd509a02c8597df8a18ba1527a5895
Tom Solchan Ahn <tom@channel.io>
2024-08-02 16:46:22 +0900

    [Etc] Pressed Color 관련 주석 추가

--- BezierSwift/Sources/Components/Button/BezierButtonStyle.swift
+++ BezierSwift/Sources/Components/Button/BezierButtonStyle.swift
@@ -14,6 +14,7 @@ struct BezierButtonStyle: ButtonStyle {
     self.configuration = configuration
   }
   
+  // TODO: Pressed Color 논의가 끝나지 않은 상태라 이번 PR에서 제외합니다. by Tom 2024.08.02
   func makeBody(configuration: Configuration) -> some View {
     configuration.label
       .background(
