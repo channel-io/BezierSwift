66ca92813c1018c9d4f028c8d6ea098c36a531f9
Tom Solchan Ahn <tom@channel.io>
2024-08-18 09:42:58 +0900

    [Feature] IconButton 추가

---
+++ BezierSwift/Sources/Components/IconButton/BezierIconButtonConfiguration.swift
@@ -0,0 +1,150 @@
+//
+//  BezierIconButtonConfiguration.swift
+//  
+//
+//  Created by Tom on 8/16/24.
+//
+
+import Foundation
+
+// - MARK: BezierIconButtonConfiguration
+/// - Parameters:
+///   - size: Button은 `xsmall`, `small`, `medium`,` large`, `xlarge` 5개의 사이즈를 가질 수 있습니다. `medium`이 가장 보편적으로 사용되며, 페이지 내의 중요도와 시각적 균형에 맞게 적절하게 사용합니다. 기본값은 `large` 입니다.
+///   - variant: 스타일에는 위계와 형태가 모두 포함됩니다. `primary`, `secondary`, `tertiary` 는 위계를 나타내는 표현으로 적힌 순서로 낮아집니다. 화면 내에서 액션의 중요도에 따라 버튼의 Hierachy를 다르게 사용합니다. 또한 `primary` 는 가장 중요한 버튼에 사용합니다. 일반적으로 한 화면에서 1개 사용을 권장하며, 너무 많이 사용하지 않도록 해주세요.
+///   - Color: Semantic 그룹에 속하는 모든 컬러를 사용할 수 있습니다.
+///   - Shape: Shape는 `rectangle`, `circle` 2개의 모양을 가질 수 있습니다.
+///   - Dropdown: 드랍다운 등의 인터렉션을 표현하기 위해 사용됩니다. 따라서 chevron 으로 아이콘이 고정되어 있습니다.
+public struct BezierIconButtonConfiguration: Equatable {
+  // MARK: Variant
+  public enum Variant {
+    case primary
+    case secondary
+    case tertiary
+  }
+  
+  // MARK: Size
+  public enum Size {
+    case xsmall
+    case small
+    case medium
+    case large
+    case xlarge
+  }
+  
+  // MARK: Color
+  public enum Color {
+    case blue
+    case cobalt
+    case red
+    case orange
+    case green
+    case pink
+    case purple
+    case darkGrey
+    case lightGrey
+    case absoluteWhite
+  }
+  
+  // MARK: Shape
+  public enum Shape {
+    case rectangle
+    case circle
+  }
+  
+  // MARK: Properties
+  let variant: Variant
+  let color: Color
+  let size: Size
+  let shape: Shape
+  
+  var contentLength: CGFloat {
+    switch self.size {
+    case .xsmall: return 16
+    case .small, .medium, .large: return 20
+    case .xlarge: return 24
+    }
+  }
+  
+  var padding: CGFloat {
+    switch self.size {
+    case .xsmall: return 4
+    case .small: return 6
+    case .medium: return 10
+    case .large: return 14
+    case .xlarge: return 15
+    }
+  }
+  
+  var cornerRadius: CGFloat {
+    switch self.shape {
+    case .rectangle:
+      switch self.size {
+      case .xsmall: return 8
+      case .small: return 10
+      case .medium: return 12
+      case .large: return 14
+      case .xlarge: return 16
+      }
+    case .circle: return (self.contentLength + (self.padding * 2)) / 2
+    }
+  }
+  
+  var backgroundColor: BezierColor {
+    guard self.variant != .tertiary else { return .bgWhiteAlphaTransparent }
+    
+    let isPrimary = self.variant == .primary
+    
+    switch self.color {
+    case .blue:
+      return isPrimary ? .primaryBgNormal : .primaryBgLightest
+    case .cobalt:
+      return isPrimary ? .accentBgNormal : .accentBgLightest
+    case .red:
+      return isPrimary ? .criticalBgNormal : .criticalBgLightest
+    case .green:
+      return isPrimary ? .successBgNormal : .successBgLightest
+    case .orange:
+      return isPrimary ? .warningBgNormal : .warningBgLightest
+    case .pink:
+      return isPrimary ? .bgPinkNormal : .bgPinkLightest
+    case .purple:
+      return isPrimary ? .bgPurpleNormal : .bgPurpleLightest
+    case .darkGrey:
+      return isPrimary ? .bgGreyDarkest : .bgBlackLighter
+    case .lightGrey:
+      return isPrimary ? .bgBlackDark : .bgBlackLighter
+    case .absoluteWhite:
+      return isPrimary ? .bgAbsoluteWhiteDark : .bgAbsoluteWhiteLightest
+    }
+  }
+  
+  var loaderSize: BezierLoaderConfiguration.Size {
+    switch self.size {
+    case .xsmall, .small: return .xxsmall
+    case .medium, .large: return .xsmall
+    case .xlarge: return .small
+    }
+  }
+  
+  var loaderVariant: BezierLoaderConfiguration.Variant {
+    return self.variant == .primary ? .onOverlay : .secondary
+  }
+  
+  // MARK: Initializer
+  /// - Parameters:
+  ///   - size: Button은 `xsmall`, `small`, `medium`,` large`, `xlarge` 5개의 사이즈를 가질 수 있습니다. `large`가 가장 보편적으로 사용되며, 페이지 내의 중요도와 시각적 균형에 맞게 적절하게 사용합니다. 기본값은 `large` 입니다.
+  ///   - variant: 스타일에는 위계와 형태가 모두 포함됩니다. `primary`, `secondary`, `tertiary` 는 위계를 나타내는 표현으로 적힌 순서로 낮아집니다. 화면 내에서 액션의 중요도에 따라 버튼의 Hierachy를 다르게 사용합니다. 또한 `primary` 는 가장 중요한 버튼에 사용합니다. 일반적으로 한 화면에서 1개 사용을 권장하며, 너무 많이 사용하지 않도록 해주세요.
+  ///   - Color: Semantic 그룹에 속하는 모든 컬러를 사용할 수 있습니다.
+  public init(
+    variant: Variant,
+    color: Color,
+    size: Size = .large,
+    shape: Shape
+  ) {
+    self.variant = variant
+    self.color = color
+    self.size = size
+    self.shape = shape
+  }
+}
+
---
+++ BezierSwift/Sources/Components/IconButton/BezierIconButtonStyle.swift
@@ -0,0 +1,27 @@
+//
+//  BezierIconButtonStyle.swift
+//
+//
+//  Created by Tom on 8/16/24.
+//
+
+import SwiftUI
+
+struct BezierIconButtonStyle: ButtonStyle {
+  let configuration: BezierIconButtonConfiguration
+  
+  init(configuration: BezierIconButtonConfiguration) {
+    self.configuration = configuration
+  }
+  
+  // TODO: Pressed Color 논의가 끝나지 않은 상태라 이번 PR에서 제외합니다. by Tom 2024.08.02
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .background(
+        RoundedRectangle(cornerRadius: self.configuration.cornerRadius, style: .circular)
+          .foregroundColor(
+            configuration.isPressed ? self.configuration.backgroundColor.color: self.configuration.backgroundColor.color
+          )
+      )
+  }
+}
