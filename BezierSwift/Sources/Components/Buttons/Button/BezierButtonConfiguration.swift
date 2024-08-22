68ec117d32bbcfa25dffb1d3ab10b8c3ffe622d8
Tom Solchan Ahn <tom@channel.io>
2024-08-22 16:26:36 +0900

    [Fix] 누락된 주석 추가 및 수정

--- BezierSwift/Sources/Components/Buttons/Button/BezierButtonConfiguration.swift
+++ BezierSwift/Sources/Components/Buttons/Button/BezierButtonConfiguration.swift
@@ -11,7 +11,7 @@ import Foundation
 /// - Parameters:
 ///   - size: Button은 `xsmall`, `small`, `medium`,` large`, `xlarge` 5개의 사이즈를 가질 수 있습니다. `medium`이 가장 보편적으로 사용되며, 페이지 내의 중요도와 시각적 균형에 맞게 적절하게 사용합니다. 기본값은 `large` 입니다.
 ///   - variant: 스타일에는 위계와 형태가 모두 포함됩니다. `primary`, `secondary`, `tertiary` 는 위계를 나타내는 표현으로 적힌 순서로 낮아집니다. 화면 내에서 액션의 중요도에 따라 버튼의 Hierachy를 다르게 사용합니다. 또한 `primary` 는 가장 중요한 버튼에 사용합니다. 일반적으로 한 화면에서 1개 사용을 권장하며, 너무 많이 사용하지 않도록 해주세요.
-///   - Color: Semantic 그룹에 속하는 모든 컬러를 사용할 수 있습니다.
+///   - color: Semantic 그룹에 속하는 모든 컬러를 사용할 수 있습니다.
 public struct BezierButtonConfiguration: Equatable {
   // MARK: Variant
   public enum Variant {
@@ -211,7 +211,7 @@ public struct BezierButtonConfiguration: Equatable {
   /// - Parameters:
   ///   - size: Button은 `xsmall`, `small`, `medium`,` large`, `xlarge` 5개의 사이즈를 가질 수 있습니다. `medium`이 가장 보편적으로 사용되며, 페이지 내의 중요도와 시각적 균형에 맞게 적절하게 사용합니다. 기본값은 `large` 입니다.
   ///   - variant: 스타일에는 위계와 형태가 모두 포함됩니다. `primary`, `secondary`, `tertiary` 는 위계를 나타내는 표현으로 적힌 순서로 낮아집니다. 화면 내에서 액션의 중요도에 따라 버튼의 Hierachy를 다르게 사용합니다. 또한 `primary` 는 가장 중요한 버튼에 사용합니다. 일반적으로 한 화면에서 1개 사용을 권장하며, 너무 많이 사용하지 않도록 해주세요.
-  ///   - Color: Semantic 그룹에 속하는 모든 컬러를 사용할 수 있습니다.
+  ///   - color: Semantic 그룹에 속하는 모든 컬러를 사용할 수 있습니다.
   public init(
     text: String,
     variant: Variant,
