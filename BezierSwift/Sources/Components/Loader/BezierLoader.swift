002cc785843074d945efdbd84ea57ef01c45fb94
Tom Solchan Ahn <tom@channel.io>
2024-09-11 20:05:35 +0900

    [Improvement] BezierLoader 주석 수정

--- BezierSwift/Sources/Components/Loader/BezierLoader.swift
+++ BezierSwift/Sources/Components/Loader/BezierLoader.swift
@@ -9,15 +9,20 @@ import SwiftUI
 
 /// NOTE: SwiftUI 로 구현한 BezierLoader의 경우 Animation 사이클이 부모뷰 사이클과 같이 동작하기 때문에 범용적인 컴포넌트로 사용이 어려운 문제가 있었습니다.
 /// UIKit으로 구현한 Loader를 UIViewRepresentable 로 감싸서 사용하며, 정확한 사이즈 사용을 위해 SwiftUI View로 한번 더 감싸서 사용합니다. by Tom 2024.08.01
+// - MARK: BezierLoader
 public struct BezierLoader: View {
   public typealias Configuration = BezierLoaderConfiguration
   
   private let configuration: Configuration
   
+  // MARK: Initializer
+  /// - Parameters:
+  ///   - configuration: 버튼의 스타일과 모양을 정의하는 `BezierLoaderConfiguration` 객체입니다.
   public init(configuration: Configuration) {
     self.configuration = configuration
   }
   
+  // MARK: Body
   public var body: some View {
     Representable(configuration: self.configuration)
       .compositingGroup()
@@ -25,6 +30,7 @@ public struct BezierLoader: View {
   }
 }
 
+// - MARK: Representable
 extension BezierLoader {
   struct Representable: UIViewRepresentable {
     private let configuration: Configuration
