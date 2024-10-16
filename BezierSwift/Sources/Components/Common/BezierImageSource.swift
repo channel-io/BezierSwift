7b87698537569598ec0608e7fc48b11892683e02
Tom Solchan Ahn <tom@channel.io>
2024-10-16 15:49:10 +0900

    [Fix] EmptyView 제거

--- BezierSwift/Sources/Components/Common/BezierImageSource.swift
+++ BezierSwift/Sources/Components/Common/BezierImageSource.swift
@@ -31,7 +31,7 @@ public enum BezierImageSource: View {
             fallback
               .resizable()
           } else {
-            EmptyView()
+            Color.clear
           }
         }
       }
